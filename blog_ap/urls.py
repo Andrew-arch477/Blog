b8f3f3bcabaea8fd0bcbe4884c8b3d7e67f2b37f
Andrew-arch477 <forandrew1985@gmail.com>
2025-08-07 21:04:11 +0300

    Fixed urls

--- blog_ap/urls.py
+++ blog_ap/urls.py
@@ -19,5 +19,5 @@ from django.urls import path, include
 
 urlpatterns = [
     path('admin/', admin.site.urls),
-    path('', include('task_tracking_system.urls')),
+    path('', include('blog.urls')),
 ]
