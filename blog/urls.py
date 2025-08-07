124e13158c9d3ea042175c7f158a870e8ea1e686
Andrew-arch477 <forandrew1985@gmail.com>
2025-08-07 21:52:19 +0300

    Created main page.

--- blog/urls.py
+++ blog/urls.py
@@ -3,6 +3,9 @@ from django.urls import path, include
 from . import views
 
 urlpatterns = [
+
+    path('', views.Main_Page_View.as_view(), name='main_page'),
+
     path('login/', views.Login_View.as_view(), name='login_page'),
     path('logout/', views.Logout_View.as_view(), name='logout_page'),
     path('registration/', views.Registration_View.as_view(), name='registration_page'),
