7a23589d046e4e3237fc4a97965c243aab381be4
Andrew-arch477 <forandrew1985@gmail.com>
2025-08-15 13:41:17 +0300

    Updated admin file.

--- blog/admin.py
+++ blog/admin.py
@@ -1,3 +1,10 @@
 from django.contrib import admin
+from .models import Article, Comment, User, Announcement, Category, Tag, Rating
 
-# Register your models here.
+admin.site.register(Article)
+admin.site.register(Comment)
+admin.site.register(User)
+admin.site.register(Announcement)
+admin.site.register(Category)
+admin.site.register(Rating)
+admin.site.register(Tag)
